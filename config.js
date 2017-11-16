ffbc786de6537cb29fc2134d75c07a9db8f4b2f8
Arnav Gupta <arnav@codingblocks.com>
2017-11-16 18:25:08 +0530

    more config from env
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

--- config.js
+++ config.js
@@ -1,15 +1,20 @@
 exports = module.exports = {
   AMQP: {
-    HOST: "localhost",
-    PORT: "5672"
+    HOST: process.env.AMQP_HOST || "localhost",
+    PORT: process.env.AMQP_PORT || "5672"
   },
   RUNBOX: {
-    DIR: "/tmp/runbox"
+    DIR: process.env.RUNBOX_DIR || "/tmp/runbox"
   },
   LANGS: {
+    'java8': {
+      SOURCE_FILE: 'Main.java',
+      CPU_SHARE: '1.2',
+      MEM_LIMIT: '80m',
+    },
     'cpp': {
       SOURCE_FILE: 'source.cpp',
-      CPU_SHARE: "1.0",
+      CPU_SHARE: "0.8",
       MEM_LIMIT: '20m',
     },
     'c': {
--- dist/config.js
+++ dist/config.js
@@ -9,12 +9,12 @@ exports = module.exports = {
     LANGS: {
         'cpp': {
             SOURCE_FILE: 'source.cpp',
-            CPU_SHARE: "0.5",
+            CPU_SHARE: "0.8",
             MEM_LIMIT: '20m',
         },
         'c': {
             SOURCE_FILE: 'source.c',
-            CPU_SHARE: "1.0",
+            CPU_SHARE: "0.5",
             MEM_LIMIT: '20m'
         }
     }
