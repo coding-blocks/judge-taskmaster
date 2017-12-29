f7c55d5235e2005dc0698f186ad8e3fa2c8b726e
Arnav Gupta <arnav@codingblocks.com>
2017-12-29 22:37:51 +0530

    improve build file
    Signed-off-by: Arnav Gupta <arnav@codingblocks.com>

--- build_docker.sh
+++ build_docker.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
+npm install -D
+npm run build
 
 docker build -t codingblocks/judge-taskmaster .
-docker system prune
+docker system prune -f
+
 read -p "Push ? [Press enter to continue]"
 docker push codingblocks/judge-taskmaster
--- config.js
+++ config.js
@@ -12,27 +12,27 @@ exports = module.exports = {
     'java8': {
       SOURCE_FILE: 'Main.java',
       CPU_SHARE: '1.2',
-      MEM_LIMIT: '80m',
+      MEM_LIMIT: '500m',
     },
     'cpp': {
       SOURCE_FILE: 'source.cpp',
       CPU_SHARE: "0.8",
-      MEM_LIMIT: '20m',
+      MEM_LIMIT: '200m',
     },
     'c': {
       SOURCE_FILE: 'source.c',
       CPU_SHARE: "0.5",
-      MEM_LIMIT: '20m'
+      MEM_LIMIT: '100m'
     },
     'py2': {
       SOURCE_FILE: 'source.py',
-      CPU_SHARE: "0.5",
-      MEM_LIMIT: '20m'
+      CPU_SHARE: "0.8",
+      MEM_LIMIT: '200m'
     },
     'nodejs6': {
       SOURCE_FILE: 'source.js',
-      CPU_SHARE: "0.5",
-      MEM_LIMIT: '20m'
+      CPU_SHARE: "0.8",
+      MEM_LIMIT: '300m'
     }
   }
 }
