b6ce935029d3f495e20025260d236587468e01f7
PrabalSingh <praba170107045@iitg.ac.in>
2020-05-23 16:44:19 +0530

    add projectScenario run & result

--- src/tasks/scenarios/project.ts
+++ src/tasks/scenarios/project.ts
@@ -8,6 +8,7 @@ import { download } from 'utils/request'
 
 export default class ProjectScenario extends Scenario {
   async setup(currentJobDir: string, job: ProjectJob) {
+    // check this. directory might be wrong
     const problemDir = path.join(currentJobDir, 'problem')
     mkdir('-p', problemDir)
     await download(job.problem, path.join(problemDir, 'problem'))
@@ -26,16 +27,46 @@ export default class ProjectScenario extends Scenario {
         -v "${currentJobDir}":/usr/src/runbox \\
         -w /usr/src/runbox codingblocks/project-worker-"${job.lang}" \\
         /bin/judge.sh -s "${job.submissionDirs}"
-    `)
+    `);
   }
 
   async result(currentJobDir: string, job: ProjectJob): Promise<ProjectResult> {
-    // TODO
+
+    const result_code = cat(path.join(currentJobDir, 'result.code')).toString()
+
+    if (result_code) {
+      // problem hash and solution hash were not equal. // error
+      return {
+        id: job.id,
+        stderr: cat(path.join(currentJobDir, 'result.stderr')).toString(),
+        stdout: '',
+        code: parseInt(result_code),
+        time: 1,
+        score: 0
+      }
+    }
+
+    const build_stderr = cat(path.join(currentJobDir, 'build.stderr')).toString()
+    const stderr = build_stderr || cat((path.join(currentJobDir, 'run.stderr')).toString())
+
+    if (stderr) {
+      return {
+        id: job.id,
+        stderr: build_stderr,
+        stdout: '',
+        code: 12123,
+        time: 1,
+        score: 0
+      }
+    }
+
+    const build_stdout = cat(path.join(currentJobDir, 'build.stdout')).toString()
+    const stdout = build_stdout || cat(path.join(currentJobDir, 'run.stdout')).toString()
 
     return {
       id: job.id,
       stderr: '',
-      stdout: '',
+      stdout: stdout,
       time: 0,
       code: 0,
       score: 0
